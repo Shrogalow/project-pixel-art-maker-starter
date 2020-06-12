5c733b73aafdc80b6d6ddda116485b15993772d7
Shrogalow <62358487+Shrogalow@users.noreply.github.com>
2020-06-12 22:14:12 +0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,19 @@
 // Select color input
+const colorPicker = document.querySelector('colorPicker');
 // Select size input
+const rowsNumber = document.querySelector('inputHeight');
+const cellsNumber = document.querySelector('inputWidth');
+const pixelCanvas = document.querySelectorAll('pixelCanvas');
+const form = document.getElementById('sizePicker');
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
 // Your code goes here!
-
+for(let i = 0; i<= rowsNumber.value; i++){
+    const row = pixelCanvas.insertRow(0);
+    for(let j = 0; j<=cellsNumber.value; j++){
+        row.cellsNumber(0);
+    }
+}
 }
